0a0f258b9c2fcd45ae80cd1c3f895bcebe1f8ca8
HookDev-arch <ip.docenko_v.a@bk.ru>
2025-03-23 12:32:22 +0600

    Update!

--- banner.sh
+++ banner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo -ne "\\033[2J\033[3;1f"
-eval "cat ~/hook/assets/banner.txt"
-printf "\n\n\033[1;32mHikka is running!\033[0m"
+eval "cat ~/Hook/assets/banner.txt"
+printf "\n\n\033[1;32mHook is running!\033[0m"
